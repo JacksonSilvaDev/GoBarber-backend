9d4407fb037014e4690f003f0b58977af5179370
Jackson Silva <jacksonsilvadev@gmail.com>
2020-08-30 11:22:05 -0300

    Add relationship beetwen User and Appointments

--- src/database/migrations/1598788999170-CreateAppointments.ts
+++ src/database/migrations/1598788999170-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1598788999170
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',

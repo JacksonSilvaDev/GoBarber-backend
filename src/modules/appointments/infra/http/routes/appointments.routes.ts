b2d494181a7ca5b4735510385314ba6a66ef27b8
Jackson Silva <jacksonsilvadev@gmail.com>
2020-09-06 13:22:07 -0300

    Refactored structure for DDD

---
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
+const appointmentsRouter = Router();
+const appointmentsReposity = new AppointmentsRepository();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+// appointmentsRouter.get('/', async (req, res) => {
+//   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+//   const appointments = await appointmentsRepository.find();
+
+//   return res.json(appointments);
+// });
+
+appointmentsRouter.post('/', async (req, res) => {
+  const { provider_id, date } = req.body;
+
+  const parsedDate = parseISO(date);
+
+  const createAppointment = new CreateAppointmentService(appointmentsReposity);
+
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
+
+  return res.json(appointment);
+});
+
+export default appointmentsRouter;

f2b43544a61258114197c58cf07eb5ec4ef2d00c
Jackson Silva <jacksonsilvadev@gmail.com>
2020-09-06 16:32:26 -0300

    Added a constrollers to application

---
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -0,0 +1,24 @@
+import { Response, Request } from 'express';
+import { container } from 'tsyringe';
+import { parseISO } from 'date-fns';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+export default class AppointmentsController {
+  /**
+   * create
+   */
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { provider_id, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = container.resolve(CreateAppointmentService);
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return res.json(appointment);
+  }
+}
